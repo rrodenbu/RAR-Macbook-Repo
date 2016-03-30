489ec08af7bb54869c2f34dc1e2c54a076810860
rrodenbu <riley.rodenburg@me.com>
2016-03-30 08:56:04 -0400

    It's working? Added "Hello world".

---
+++ Java Practice/HelloWorld/src/com/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.example.helloworld;
+
+/**
+ * Created by rrodenbu on 3/27/16.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Java Practice/HelloWorld/src/com/example/helloworld/HelloWorldTest.groovy
@@ -0,0 +1,10 @@
+package com.example.helloworld
+
+/**
+ * Created by rrodenbu on 3/27/16.
+ */
+class HelloWorldTest extends groovy.util.GroovyTestCase {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
