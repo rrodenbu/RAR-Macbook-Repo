63821efb7a0e8114155de9ba5ef72efd85bc19d7
rrodenbu <riley.rodenburg@me.com>
2016-04-01 08:39:28 -0400

    Moving head.

--- Java-Practice/Snake/src/snake/RenderPanel.java
+++ Java-Practice/Snake/src/snake/RenderPanel.java
@@ -5,16 +5,26 @@ import java.awt.*;
 
 /**
  * Created by rrodenbu on 3/30/16.
+ * Creates the actual game, in a new screen.
  */
 @SuppressWarnings("serial")
 public class RenderPanel extends JPanel {
     public static int curColor = 0;
+
     public static Color green = new Color(13434675);
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);     //set color
         g.setColor(green);     //also: new Color(0)
         g.fillRect(0, 0, 800, 700);  //set size of fill
+        Snake snake = Snake.snake;
+        g.setColor(Color.RED);
+        for (Point point: snake.snakeParts){ //loop through all the parts in snakes body (array of parts)
+            System.out.println("Printin snake body");
+            g.fillRect(point.x * Snake.SCALE, point.y * snake.SCALE, snake.SCALE, snake.SCALE);
+        }
+        g.fillRect(snake.head.x, snake.head.y, Snake.SCALE, Snake.SCALE);
         //curColor++;
     }
 }
--- Java-Practice/Snake/src/snake/Snake.java
+++ Java-Practice/Snake/src/snake/Snake.java
@@ -5,11 +5,14 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by rrodenbu on 3/30/16.
  */
 public class Snake implements ActionListener {
+
+    //Variables declared up here (outside of Snake function) are public, any function can acess
 
     public JFrame jframe;
 
@@ -23,14 +26,21 @@ public class Snake implements ActionListener {
 
     public ArrayList<Point> snakeParts = new ArrayList<Point>();
 
-    public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3;
+    public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, SCALE = 10;
+    //SCALE = dimensions of one part of the snake
 
-    public int ticks = 0, direction = DOWN;
+    public int ticks = 0, direction = DOWN, score, tailLength;
 
-    public Point head;
+    public Point head, cherry;
+
+    public Random random;
+
+    public boolean over = false; //checks if snake is about to go out of bounds
+
+    public Dimension dim;
 
     public Snake() {
-        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize(); //get computers screen size
+        dim = Toolkit.getDefaultToolkit().getScreenSize(); //get computers screen size
         jframe = new JFrame("Snake!");                               //create new window with title: Snake!
         jframe.setVisible(true);                                     //make window visible
         jframe.setSize(800,800);                                     //set windows dimnenstions
@@ -38,6 +48,8 @@ public class Snake implements ActionListener {
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       //make it possible to close window
         jframe.add(renderPanel = new RenderPanel());                 //create new panel
         timer.start();
+        random = new Random();
+        cherry = new Point(dim.width/ SCALE, dim.height / SCALE);
         head = new Point(0,0);
     }
 
@@ -45,10 +57,38 @@ public class Snake implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         renderPanel.repaint(); //will repaint window everytime this function is called
         ticks++;
-        if (ticks % 10 == 0) { //every half-second (10 ticks)
-            if(direction = UP)
-                snakeParts.add(new Point(head.x, head.y +));
-            snakeParts.add();
+
+        if (ticks % 10 == 0 && head != null && over != true) { //every half-second (10 ticks)
+            snakeParts.add(new Point(head.x, head.y)); //adding new head to array
+            //x+ = right y+ = down
+            if(direction == UP)
+                if (head.y - 1 > 0) //NOT at top of screen
+                    head = new Point(head.x, head.y - 1); //change location of head up one point
+                else //At top of screen
+                    over = true;
+            if(direction == DOWN)
+                if (head.y + 1 < dim.height / SCALE) //NOT at bottom of screen
+                    head = new Point(head.x, head.y + 1); //change location of head down one point
+                else //At bottom of screen
+                    over = true;
+            if(direction == LEFT)
+                if (head.x - 1 > 0) //NOT at left edge of screen
+                    head = new Point(head.x - 1, head.y); //change location of head left one point
+                else //At left edge of screen
+                    over = true;
+            if(direction == RIGHT)
+                if (head.x + 1 < dim.width / SCALE) //NOT at right edge of screen
+                    head = (new Point(head.x + 1, head.y)); //change location of head right one point
+                else    //At right edge of screen
+                    over = true;
+            snakeParts.remove(0); //remove the last part of the snake, otherwise it will grow.
+            if(cherry != null) {
+                if(head.equals(cherry)) {//if(head.x == cherry.x && head.y == cherry.y
+                    score += 10; //Add
+                    tailLength++;
+                    cherry.setLocation(dim.width/SCALE, dim.height / SCALE);
+                }
+            }
         }
     }
 
