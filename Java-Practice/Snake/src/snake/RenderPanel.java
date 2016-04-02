3f28f181f799e50b08d0f881e568ddd62d503a09
rrodenbu <riley.rodenburg@me.com>
2016-04-01 22:41:47 -0400

    Working but the orientations are all wrong...

--- Java-Practice/Snake/src/snake/RenderPanel.java
+++ Java-Practice/Snake/src/snake/RenderPanel.java
@@ -8,23 +8,49 @@ import java.awt.*;
  * Creates the actual game, in a new screen.
  */
 @SuppressWarnings("serial")
-public class RenderPanel extends JPanel {
-    public static int curColor = 0;
+public class RenderPanel extends JPanel
+{
+        public static final Color GREEN = new Color(13434675); //set games background color
+        public static final Color DULLGREY = new Color(196196255);
 
-    public static Color green = new Color(13434675);
+        @Override
+        protected void paintComponent(Graphics g)
+        {
+                super.paintComponent(g);
 
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);     //set color
-        g.setColor(green);     //also: new Color(0)
-        g.fillRect(0, 0, 800, 700);  //set size of fill
-        Snake snake = Snake.snake;
-        g.setColor(Color.RED);
-        for (Point point: snake.snakeParts){ //loop through all the parts in snakes body (array of parts)
-            System.out.println("Printin snake body");
-            g.fillRect(point.x * Snake.SCALE, point.y * snake.SCALE, snake.SCALE, snake.SCALE);
+                //set background color
+                g.setColor(GREEN);     //also: new Color(0)
+                g.fillRect(0, 0, 400, 400);  //set size of fill
+
+                //set snake head and parts color
+                g.setColor(Color.BLACK);
+                Snake snake = Snake.snake;
+                for (Point point: snake.snakeParts){ //loop through all the parts in snakes body (array of parts)
+                    System.out.println("(" + point.x + "," + point.y + ")");
+                    System.out.println("   (" + point.x*snake.SCALE + "," + point.y*snake.SCALE + ")");
+                    g.fillRect((point.x, point.y, snake.SCALE, snake.SCALE);
+                }
+                g.fillRect(snake.head.x, snake.head.y, Snake.SCALE, Snake.SCALE);
+
+                //set snakes food color
+                g.setColor(Color.RED);
+                g.fillRect(snake.cherry.x, snake.cherry.y, snake.SCALE, snake.SCALE);
+
+                //Sets text and text color
+                g.setColor(DULLGREY);
+                String string = "Score: " + snake.score + ", Length: " + snake.tailLength + "Time: " + snake.time / 20;
+                g.drawString(string, (int) (getWidth() / 2 - string.length() * 2.5f), 10);
+
+                string = "Game Over!";
+                if (snake.over)
+                {
+                    g.drawString(string, (int) (getWidth() / 2 - string.length() * 2.5f), (int) snake.dim.getHeight() / 4);
+                }
+
+                string = "Paused!";
+                if (snake.paused && !snake.over)
+                {
+                    g.drawString(string, (int) (getWidth() / 2 - string.length() * 2.5f), (int) snake.dim.getHeight() / 4);
+                }
         }
-        g.fillRect(snake.head.x, snake.head.y, Snake.SCALE, Snake.SCALE);
-        //curColor++;
-    }
 }
--- Java-Practice/Snake/src/snake/Snake.java
+++ Java-Practice/Snake/src/snake/Snake.java
@@ -1,100 +1,171 @@
 package snake;
 
-import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 import java.util.Random;
+
+import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by rrodenbu on 3/30/16.
  */
-public class Snake implements ActionListener {
+public class Snake implements ActionListener, KeyListener {
 
-    //Variables declared up here (outside of Snake function) are public, any function can acess
+        //Variables declared up here (outside of Snake function) are public, any function can acess
+        //private: anything in this class can see it
+        //public: anything in this package can see it
+        //public static a: anything anywhere can access it
 
-    public JFrame jframe;
+        public static Snake snake; //allow snake to be accessed anywhere not just in main (where it is declared).
 
-    public RenderPanel renderPanel;
+        public JFrame jframe;
 
-    public static Snake snake; //allow snake to be accessed anywhere not just in main (where it is declared).
+        public RenderPanel renderPanel;
 
-    public Timer timer = new Timer(20, this); //frequency to perform actionPerformed
+        public Timer timer = new Timer(20, this); //frequency to perform actionPerformed
 
-    public Toolkit toolkit;
+        public ArrayList<Point> snakeParts = new ArrayList<Point>();
 
-    public ArrayList<Point> snakeParts = new ArrayList<Point>();
+        public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, SCALE = 20;
+        //SCALE = dimensions of one part of the snake
 
-    public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, SCALE = 10;
-    //SCALE = dimensions of one part of the snake
+        public int ticks = 0, direction = DOWN, score, tailLength = 0, time;
 
-    public int ticks = 0, direction = DOWN, score, tailLength;
+        public Point head, cherry;
 
-    public Point head, cherry;
+        public Random random;
 
-    public Random random;
+        public boolean over = false, paused; //checks if snake is about to go out of bounds
 
-    public boolean over = false; //checks if snake is about to go out of bounds
+        public Dimension dim;
 
-    public Dimension dim;
+        public Snake()
+        {
+                dim = Toolkit.getDefaultToolkit().getScreenSize(); //get computers screen size
+                jframe = new JFrame("Snake!"); //create new window with title: Snake!
+                jframe.setVisible(true); //make window visible
+                jframe.setSize(400 , 400); //set windows dimensions
+                jframe.setResizable(false); //prevents cheat where you could resize the screen
+                jframe.setLocation(dim.width / 2 - jframe.getWidth() / 2, dim.height / 2 - jframe.getHeight() / 2); //center
+                jframe.add(renderPanel = new RenderPanel());//create new panel
+                jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       //make it possible to close window
+                jframe.addKeyListener(this);
+                startGame();
+        }
 
-    public Snake() {
-        dim = Toolkit.getDefaultToolkit().getScreenSize(); //get computers screen size
-        jframe = new JFrame("Snake!");                               //create new window with title: Snake!
-        jframe.setVisible(true);                                     //make window visible
-        jframe.setSize(800,800);                                     //set windows dimnenstions
-        jframe.setLocation(dim.width / 2 - jframe.getWidth() / 2, dim.height / 2 - jframe.getHeight() / 2); //center
-        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);       //make it possible to close window
-        jframe.add(renderPanel = new RenderPanel());                 //create new panel
-        timer.start();
-        random = new Random();
-        cherry = new Point(dim.width/ SCALE, dim.height / SCALE);
-        head = new Point(0,0);
-    }
+        //resets to original game state: snake top left corner going down
+        //  score = 0, time = 0, new cherry, etc.
+        public void startGame ()
+        {
+                over = false;
+                paused = false;
+                score = 0;
+                time = 0;
+                tailLength = 5;
+                ticks = 0;
+                direction = DOWN;
+                head = new Point(0, -1);
+                snakeParts.clear();
+                random = new Random();
+                cherry = new Point(random.nextInt(391), random.nextInt(368));
+                timer.start();
+        }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        renderPanel.repaint(); //will repaint window everytime this function is called
-        ticks++;
+        @Override
+        public void actionPerformed(ActionEvent e)
+        {
+                renderPanel.repaint(); //will repaint window everytime this function is called
+                ticks ++;
+                int snakeSpeed = 1;
 
-        if (ticks % 10 == 0 && head != null && over != true) { //every half-second (10 ticks)
-            snakeParts.add(new Point(head.x, head.y)); //adding new head to array
-            //x+ = right y+ = down
-            if(direction == UP)
-                if (head.y - 1 > 0) //NOT at top of screen
-                    head = new Point(head.x, head.y - 1); //change location of head up one point
-                else //At top of screen
-                    over = true;
-            if(direction == DOWN)
-                if (head.y + 1 < dim.height / SCALE) //NOT at bottom of screen
-                    head = new Point(head.x, head.y + 1); //change location of head down one point
-                else //At bottom of screen
-                    over = true;
-            if(direction == LEFT)
-                if (head.x - 1 > 0) //NOT at left edge of screen
-                    head = new Point(head.x - 1, head.y); //change location of head left one point
-                else //At left edge of screen
-                    over = true;
-            if(direction == RIGHT)
-                if (head.x + 1 < dim.width / SCALE) //NOT at right edge of screen
-                    head = (new Point(head.x + 1, head.y)); //change location of head right one point
-                else    //At right edge of screen
-                    over = true;
-            snakeParts.remove(0); //remove the last part of the snake, otherwise it will grow.
-            if(cherry != null) {
-                if(head.equals(cherry)) {//if(head.x == cherry.x && head.y == cherry.y
-                    score += 10; //Add
-                    tailLength++;
-                    cherry.setLocation(dim.width/SCALE, dim.height / SCALE);
+                if (ticks % snakeSpeed == 0 && head != null && over != true && !paused)
+                { //every half-second (10 ticks)
+                        snakeParts.add(new Point(head.x, head.y));
+                        //x+ = right y+ = down
+                        if(direction == UP)
+                            if (head.y - 1 >= 0 && noTailAt(head.x, head.y -1)) //NOT at top of screen
+                                head = new Point(head.x, head.y - 1); //change location of head up one point
+                            else //At top of screen
+                                over = true;
+
+                        if(direction == DOWN)
+                            if (head.y + 1 < 368 && noTailAt(head.x, head.y + 1)) //NOT at bottom of screen
+                                head = new Point(head.x, head.y + 1); //change location of head down one point
+                            else //At bottom of screen
+                                over = true;
+
+                        if(direction == LEFT)
+                            if (head.x - 1 >= 0 && noTailAt(head.x - 1, head.y)) //NOT at left edge of screen
+                                head = new Point(head.x - 1, head.y); //change location of head left one point
+                            else //At left edge of screen
+                                over = true;
+
+                        if(direction == RIGHT)
+                            if (head.x + 1 < 391 && noTailAt(head.x + 1, head.y)) //NOT at right edge of screen
+                                head = new Point(head.x + 1, head.y); //change location of head right one point
+                            else    //At right edge of screen
+                                over = true;
+
+                        //snakeParts.add(new Point(head.x, head.y)); //adding new head to array
+
+                        if(snakeParts.size() > tailLength) //Head moved need to remove tail.
+                            snakeParts.remove(0);
+
+                        if(cherry != null) {
+                            if(head.equals(cherry)) {//if(head.x == cherry.x && head.y == cherry.y
+                                score += 10; //i.e. add to length of snake
+                                tailLength++;
+                                cherry.setLocation(random.nextInt(391), random.nextInt(368));
+                            }
+                        }
+                }
+        }
+
+        public boolean noTailAt(int x, int y)
+        {
+            for(Point point : snakeParts) //loop through all snake parts
+            {
+                if (point.equals(new Point(x, y)))
+                {
+                    return false;
                 }
             }
+            return true;
         }
-    }
 
-    public static void main(String[] args){
-        snake = new Snake(); //create new window
-    }
+        public static void main(String[] args)
+        {
+            snake = new Snake(); //create new window
+        }
 
 
+        @Override
+        public void keyTyped(KeyEvent e) {
+        }
+
+        @Override
+        public void keyPressed(KeyEvent e) { //keyboard button pressed (passed in as e)
+            int i = e.getKeyCode(); //retrieving the key that was pressed
+            if (i == KeyEvent.VK_LEFT && direction != RIGHT) //'A' key is left and checking to make your turning back on self
+                direction = LEFT;
+            if (i == KeyEvent.VK_RIGHT && direction != LEFT) // 'D' key is right
+                direction = RIGHT;
+            if (i == KeyEvent.VK_UP && direction != DOWN) // 'W' key is up
+                direction = UP;
+            if (i == KeyEvent.VK_DOWN && direction != UP) // 'S' key is down
+                direction = DOWN;
+            if (i == KeyEvent.VK_SPACE) //GAME OVER: hit edge of screen
+                if (over)
+                    startGame(); //press space to restart
+            else
+                    paused = !paused;
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+        }
 }
