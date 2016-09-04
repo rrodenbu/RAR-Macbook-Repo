b9e8614c1def2f02d86f4ed6a7cdcb13d35ae3a6
rrodenbu <riley.rodenburg@me.com>
2016-09-04 18:31:27 -0400

    Added directions functionality.

--- iPayWhere/iPayWhere/iPayWhere/BankViewController.swift
+++ iPayWhere/iPayWhere/iPayWhere/BankViewController.swift
@@ -14,6 +14,7 @@ var selectedBank : String = ""
 class BankViewController: UIViewController {
 
     var bankSelected : String = ""
+    var selectedPin: MKPlacemark?
     
     let locationManager = CLLocationManager()
     var matchingItems: [MKMapItem] = []
@@ -34,6 +35,52 @@ class BankViewController: UIViewController {
         locationManager.requestWhenInUseAuthorization()
         locationManager.requestLocation()
     }
+    
+    func parseAddress(selectedItem:MKPlacemark) -> String {
+        
+        // put a space between "4" and "Melrose Place"
+        let firstSpace = (selectedItem.subThoroughfare != nil &&
+            selectedItem.thoroughfare != nil) ? " " : ""
+        
+        // put a comma between street and city/state
+        let comma = (selectedItem.subThoroughfare != nil || selectedItem.thoroughfare != nil) &&
+            (selectedItem.subAdministrativeArea != nil || selectedItem.administrativeArea != nil) ? ", " : ""
+        
+        // put a space between "Washington" and "DC"
+        let secondSpace = (selectedItem.subAdministrativeArea != nil &&
+            selectedItem.administrativeArea != nil) ? " " : ""
+        
+        let addressLine = String(
+            format:"%@%@%@%@%@%@%@",
+            // street number
+            selectedItem.subThoroughfare ?? "",
+            firstSpace,
+            // street name
+            selectedItem.thoroughfare ?? "",
+            comma,
+            // city
+            selectedItem.locality ?? "",
+            secondSpace,
+            // state
+            selectedItem.administrativeArea ?? ""
+        )
+        
+        return addressLine
+    }
+    
+    func getDirections(){
+            let mapItem = MKMapItem(placemark: selectedPin!)
+            let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+            mapItem.openInMapsWithLaunchOptions(launchOptions)
+    }
+    
+//    func getDirections(){
+//        if let selectedPin = selectedPin {
+//            let mapItem = MKMapItem(placemark: selectedPin)
+//            let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
+//            mapItem.openInMapsWithLaunchOptions(launchOptions)
+//        }
+//    }
 }
 
 extension BankViewController : CLLocationManagerDelegate {
@@ -80,12 +127,13 @@ extension BankViewController : CLLocationManagerDelegate {
         let latitude = item.placemark.coordinate.latitude
         let longitude = item.placemark.coordinate.longitude
         let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        let name = item.name
-        print(name)
+        let name = item.name!
         let annotation = MKPointAnnotation()
+        selectedPin = item.placemark
         annotation.coordinate = coordinate
         annotation.title = "\(name)"
-        annotation.subtitle = ""
+        
+        annotation.subtitle = parseAddress(selectedPin!)
         annotations.append(annotation)
     }
     
@@ -93,5 +141,25 @@ extension BankViewController : CLLocationManagerDelegate {
         print("error:: \(error)")
     }
     
-    
+}
+
+extension BankViewController : MKMapViewDelegate {
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView?{
+        if annotation is MKUserLocation {
+            //return nil so map view draws "blue dot" for standard user location
+            return nil
+        }
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+        
+        // Directions button
+        pinView?.canShowCallout = true
+        let smallSquare = CGSize(width: 30, height: 30)
+        let button = UIButton(frame: CGRect(origin: CGPointZero, size: smallSquare))
+        button.setBackgroundImage(UIImage(named: "car"), forState: .Normal)
+        button.addTarget(self, action: "getDirections", forControlEvents: .TouchUpInside)
+        pinView?.leftCalloutAccessoryView = button
+        return pinView
+    }
 }
